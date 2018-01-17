c998b725d470e482014a79919d41a321dbb27fa0
Aurelien HOUEL <ahouel@e3r2p11.42.fr>
2018-01-17 19:19:59 +0100

    pourquoi, norme, pourquoiiiiiiiiiiii ?

--- Machine-Virtuelle/includes/vm_struct.h
+++ Machine-Virtuelle/includes/vm_struct.h
@@ -6,7 +6,7 @@
 /*   By: ahouel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 10:08:08 by ahouel            #+#    #+#             */
-/*   Updated: 2018/01/17 19:02:49 by ahouel           ###   ########.fr       */
+/*   Updated: 2018/01/17 19:19:11 by ahouel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,19 @@ typedef struct s_pcb	t_pcb;
 **	Header de chaque files.cor
 */
 
-typedef struct	s_header
+typedef struct			s_header
 {
 	unsigned int	magic;
 	char			prog_name[PROG_NAME_LENGTH + 1];
 	unsigned int	prog_size;
 	char			comment[COMMENT_LENGTH + 1];
-}				t_header;
+}						t_header;
 
 /*
 **	Structure des operation (labels)
 */
 
-typedef struct	s_op
+typedef struct			s_op
 {
 	char	*label;
 	void	(*func)(t_vm *vm, t_pcb *proc);
@@ -45,19 +45,19 @@ typedef struct	s_op
 	int		has_ocp;
 	int		nb_byte;
 	int		addr_rest;
-}				t_op;
+}						t_op;
 
 /*
 **	op_tab de reference
 */
 
-extern t_op		g_op_tab[];
+extern t_op				g_op_tab[];
 
 /*
 **	Joueur (.cor)
 */
 
-typedef struct	s_player
+typedef struct			s_player
 {
 	char	*file_name;
 	char	*name;
@@ -66,7 +66,7 @@ typedef struct	s_player
 	int		lives_count;
 	int		last_live;
 	int		id_color;
-}				t_player;
+}						t_player;
 
 /*
 **	https://openclassrooms.com/courses/la-programmation-systeme-en-c-sous-unix/
@@ -74,7 +74,7 @@ typedef struct	s_player
 **	Processus
 */
 
-typedef struct	s_pcb
+typedef struct			s_pcb
 {
 	char			carry;
 	char			aff;
@@ -91,19 +91,19 @@ typedef struct	s_pcb
 **	Representation de la memoire (ram)
 */
 
-typedef struct	s_mem
+typedef struct			s_mem
 {
 	unsigned char	mem;
 	char			live;
 	char			store;
 	int				num;
-}				t_mem;
+}						t_mem;
 
 /*
 **	Environnement de la vm
 */
 
-typedef struct	s_vm
+typedef struct			s_vm
 {
 	char		ncurses;
 	char		pause;
@@ -122,6 +122,6 @@ typedef struct	s_vm
 	t_pcb		*proc_lst;
 	t_player	player[MAX_PLAYERS];
 	t_mem		ram[MEM_SIZE];
-}				t_vm;
+}						t_vm;
 
 #endif
